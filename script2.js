895293a7ef7f4910ad235c13bb622fc4ab77b41d
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-05 15:59:28 -0800

    added an else statement to produce the numbers that are not divisible by either 3 or 5, however, 'fizzbuzz' still does not get produced

--- script2.js
+++ script2.js
@@ -25,14 +25,17 @@
 // }
 
 for (let i = 1; i < 101; i++) {
-    if (i % 3 ) {
+    if (i % 3 == 0) {
         console.log('Fizz');
     }
-    else if (i % 5) {
+    else if (i % 5 == 0) {
         console.log('Buzz');
     }
-    else if (i % 3 && i % 5) {
-        console.log('Fizzbuzz');
+    else if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    }
+    else {
+        console.log(i);
     }
 }
 
