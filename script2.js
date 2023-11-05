b914cbb23b8c4583e2dea574fa7e788d90bfee82
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-05 15:51:52 -0800

    created the else if statement to print buzz if divisible by 5

--- script2.js
+++ script2.js
@@ -26,7 +26,10 @@
 
 for (let i = 1; i < 101; i++) {
     if (i % 3 ) {
-        console.log('Fizz')
+        console.log('Fizz');
+    }
+    else if (i % 5) {
+        console.log('Buzz');
     }
 }
 
