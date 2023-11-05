2e27772b087e301baed89a9d402c6e9b9a19aaf6
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-04 18:58:41 -0700

    added the first for loop) to make the first four rows of #'s

--- script1.js
+++ script1.js
@@ -7,3 +7,22 @@
 // ###
 // ##
 // #
+
+
+
+// adds 1 
+
+// += 1 or name of variable++
+
+ //subtracts 1
+
+// -= 1 or name of variable---
+
+
+
+for (let triangle = '#'; triangle <= 4; triangle + 1) {
+    console.log(triangle);
+}
+
+
+
--- script2.js
+++ script2.js
@@ -1,4 +1,8 @@
-// 2. Write code that prints all of the numbers from 1 to 100 with three exceptions: [1] For numbers that are divisible by 3, print “Fizz” instead of the number,  [2] for numbers that are divisible by 5, print “Buzz” instead of the number, and [3] for numbers that are divisible by both 3 and 5, print “FizzBuzz” instead of the number.
+// 2. Write code that prints all of the numbers from 1 to 100 with three exceptions: 
+// [1] For numbers that are divisible by 3, print “Fizz” instead of the number,  
+// [2] for numbers that are divisible by 5, print “Buzz” instead of the number, and 
+// [3] for numbers that are divisible by both 3 and 5, print “FizzBuzz” instead of the number.
+
 // Example output:
 // 1
 // 2
